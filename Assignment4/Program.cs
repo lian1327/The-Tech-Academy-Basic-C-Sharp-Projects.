bf98bec9e97eab4a02602eb8f25097792a49d13a
Leonard <leonardmesiera@outlook.com>
2025-01-24 11:04:14 +0800

    "Assignment 4"

---
+++ Assignment4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program 
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.WriteLine("Please enter the package weight:");
+        float inputWeight = float.Parse(Console.ReadLine());
+
+        if (inputWeight < 50)
+        {
+            Console.WriteLine("Please enter the package Width:");
+            int inputWidth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int inputHeight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int inputLength = int.Parse(Console.ReadLine());
+
+            int dimension = inputWidth + inputHeight + inputLength;
+
+            if (dimension > 50) 
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+                float price = ((inputWidth * inputHeight * inputLength) * inputWeight) / 100 ;
+                string formattedPrice =  price.ToString("N2");
+                Console.WriteLine($"Your estimated total for shipping this package is: ${formattedPrice}");
+                Console.WriteLine("Thank you!");
+            } 
+        }
+        else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+
+
+
+
+
+
+
+
+    }
+}
