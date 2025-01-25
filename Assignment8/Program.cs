65655526496d87fca122920cbc47ea2bb84bd7ba
Leonard <leonardmesiera@outlook.com>
2025-01-25 12:27:27 +0800

    "assignment 8"

---
+++ Assignment8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+       // Define a list of strings with at least two identical strings
+        List<string> stringList = new List<string> { "A", "B", "C", "D", "C" };
+
+        // Create a HashSet to keep track of seen strings
+        HashSet<string> seenStrings = new HashSet<string>();
+
+        // Loop through each item in the list
+        foreach (string item in stringList)
+        {
+            // Check if the item has already been seen
+            if (seenStrings.Contains(item))
+            {
+                // Print message if the item is a duplicate
+                Console.WriteLine($"{item} - this item is a duplicate");
+            }
+            else
+            {
+                // Add the item to the set of seen strings
+                seenStrings.Add(item);
+                // Print message if the item is unique
+                Console.WriteLine($"{item} - this item is unique");
+            }
+        }
+
+    }
+}
