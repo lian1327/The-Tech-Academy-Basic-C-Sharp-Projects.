0c564d188b69697c6e798e898b53bd5a8b4ab054
Leonard <leonardmesiera@outlook.com>
2025-01-30 09:25:17 +0800

    "completion of the demo"

---
+++ GameConsoleProject/Casino/IWalkAway.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Interfaces
+{
+    interface IWalkAway // Create an interface called IWalkAway
+    {
+        void WalkAway(Player player); // Create a method called Walkaway with one input an instance of the player object
+    } // End IWalkAway Interface
+
+}
---
+++ GameConsoleProject/Casino/TwentyOneDealer.cs
@@ -0,0 +1,23 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.TwentyOne
+{
+
+    public class TwentyOneDealer : Dealer // Inherit the Dealer class
+    {
+        /* PROPERTIES */
+
+        private List<Card> _hand = new List<Card>(); // Instance a private list of the Card class called _hand, this ensure we already have an empty list
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } } // A list of card objects to represent the Players hand, points to the private instance _hand
+        public bool Stay { get; set; } // A boolean variable to represent if the dealer wants to stay
+        public bool isBusted { get; set; } // A boolean variable to represent if the dealer has gone bust
+
+    } // End TwentyOneDealer CLASS
+
+} // End Casino NAMESPACE
